0e7e7328f8d8940164bf712b6f6d9b871a4736ed
Robert <robertneagu194@gmail.com>
2019-03-11 18:01:36 +0000

    added: vue-router

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import Fragmented from './Fragmented.vue'
 
 Vue.config.productionTip = false
-
+Vue.use(VueRouter);
 new Vue({
   render: h => h(Fragmented),
 }).$mount('#_vue-fragmented')
